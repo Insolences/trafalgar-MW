ee24892527a85237e7c2de46585cc04db6b1c192
Alex <you@example.com>
2021-01-18 11:24:25 +0200

    fix_idea

--- src/Containers/Main/Main.js
+++ src/Containers/Main/Main.js
@@ -51,7 +51,7 @@ export default class Main extends React.Component{
         )
     }
 
-    viewAll = ()=>{
+    viewAll = () =>{
         let test = this.state.viewAll
         test = !test
         this.setState(()=>{
